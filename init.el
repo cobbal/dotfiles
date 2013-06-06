2938272076de4e98026d81384696877d6baeabcd
Andrew Cobb <andrew.cobb@gmail.com>
2013-06-06 15:06:37 -0600

    el-get will now auto-install

--- init.el
+++ init.el
@@ -52,23 +52,21 @@
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
+(setq el-get-notify-type 'message)
 (unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+ (with-current-buffer
+  (url-retrieve-synchronously
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+  (let (el-get-master-branch)
+   (goto-char (point-max))
+   (eval-print-last-sexp))))
 
-(setq
- el-get-sources
- '(el-get
-   vimpulse
+
+(el-get 'sync
+ '(vimpulse
    haskell-mode
    auto-complete
    clojure-mode))
-
-(el-get 'sync)
 
 (setq initial-frame-alist '((width . 100) (height . 53) (top . 0) (left . 300)))
 (setq default-frame-alist '((width . 100) (height . 53) (top . 0)))
