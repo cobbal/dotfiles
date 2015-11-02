09275f64e69f722ecc9df3bbb4b8a6f33d4cee58
Andrew Cobb <andrew.cobb@gmail.com>
2015-11-02 18:58:06 +0000

    installer

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+WD="$(pwd -L)"
+RWD="${WD#$(readlink -f $HOME)/}"
+
+if [[ "$WD" != "$HOME/.config/dotfiles" ]]; then
+    echo
+    echo "WARNING! expected to exist in $HOME/.config/dotfiles, actually in $WD"
+    echo
+fi
+
+PROBLEMS=0
+
+function finish {
+    if [[ 0 -eq $? ]]; then
+        echo "OK"
+    else
+        echo "FAILED"
+        PROBLEMS=1
+    fi
+}
+
+function do_git {
+    local INSTL="$HOME/$2"
+    echo -n "Installing '$1' to '$INSTL'... "
+    if [[ -d "$INSTL/.git" ]]; then
+        echo "EXISTS, SKIPPING"
+    else
+        git clone "$1" "$INSTL"
+        finish
+    fi
+}
+
+function do_install {
+    local SRC="$RWD/$1"
+    local INSTL="$HOME/$2"
+    # If I ever have a home folder named "*" I deserve what I get.
+    echo -n "Linking '$SRC' to '$INSTL'... "
+    if [[ -L "$INSTL" ]]; then
+        rm "$INSTL"
+    else
+        mv "$INSTL" "$INSTL~"
+    fi
+    ln -s "$SRC" "$INSTL"
+    finish
+}
+
+do_install emacs .emacs.d
+do_install screenrc .screenrc
+do_install vimrc .vimrc
+do_install gitconfig .gitconfig
+do_install gitignore_global .gitignore_global
+do_git https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
+do_install oh-my-zsh-custom/zshrc .zshrc
+
+exit $PROBLEMS
